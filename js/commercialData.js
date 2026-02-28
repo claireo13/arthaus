60f32247d2e9f053abb388f5d06483a6bf86506f
claire <warrilow@sky.com>
2026-02-28 16:09:27 +0000

    Add files via upload

---
+++ js/commercialData.js
@@ -0,0 +1,97 @@
+// js/commercialData.js
+window.commercialGallery = [
+  {
+    style: "minimalist",
+    room: "office",
+    roomLabel: "Office",
+    title: "Minimalist conferenceroom",
+    image: "images/commercial/office/conferenceroom-minimalist-small.jpg",
+    alt: "minimalist conferenceroom"
+  },
+
+ {
+    style: "modern",
+    room: "office",
+    roomLabel: "Office",
+    title: "Modern conferenceroom",
+    image: "images/commercial/office/conferenceroom-modern-small.jpg",
+    alt: "modern conferenceroom"
+  },
+  
+  {
+    style: "minimalist",
+    room: "hotel",
+    roomLabel: "Hotel",
+    title: "hotel bedroom",
+    image: "images/commercial/hotel/hotel-bedroom-minimalist-small.webp",
+    alt: "Minimalist hotel bedroom" 
+  },
+
+{
+    style: "modern",
+    room: "hotel",
+    roomLabel: "Hotel",
+    title: "Modern hotel bedroom",
+    image: "images/commercial/hotel/hotel-bedroom-modern-medium.webp",
+    alt: "Modern hotel bedroom" 
+  },
+
+  
+{
+    style: "traditional",
+    room: "hotel",
+    roomLabel: "Hotel",
+    title: "Traditional hotel lobby",
+    image: "images/commercial/hotel/hotel-lobby-traditional-large.webp",
+    alt: "traditional hotel lobby" 
+  },
+
+  {
+    style: "modern",
+    room: "hairdressers",
+    roomLabel: "Hairdressers",
+    title: "Modern hairdressers",
+    image: "images/commercial/hairdressers/hairdresser-modern-medium.jpg",
+    alt: "modern hairdressers" 
+  },
+
+  {
+    style: "traditional",
+    room: "hairdressers",
+    roomLabel: "Hairdressers",
+    title: "Traditional hairdressers",
+    image: "images/commercial/hairdressers/hairdresser-traditional-medium.jpg",
+    alt: "traditional hairdressers" 
+  },
+
+   {
+    style: "modern",
+    room: "restaurant",
+    roomLabel: "Restaurant",
+    title: "Modern restaurant",
+    image: "images/commercial/restaurant/restaurant-modern-large.jpg",
+    alt: "modern restaurant" 
+  },
+
+  {
+    style: "modern",
+    room: "shop",
+    roomLabel: "Shop",
+    title: "Modern shop",
+    image: "images/commercial/shop/shop-modern-medium.webp",
+    alt: "modern shop" 
+  },
+
+ {
+    style: "rustic",
+    room: "shop",
+    roomLabel: "Shop",
+    title: "Modern shop",
+    image: "images/commercial/shop/shop-rustic-small.webp",
+    alt: "rustic shop" 
+  },
+
+
+
+
+];
